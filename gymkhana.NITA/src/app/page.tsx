68e8957ae5d2943b2b2da4c8636799af9fc6a3fb
Abhay Pandey <pandeyabhay967@gmail.com>
2025-12-18 19:38:24 +0530

    Update page.tsx for indexing...

--- gymkhana.NITA/src/app/page.tsx
+++ gymkhana.NITA/src/app/page.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Footer, Hero, Navbar } from "~/components";
+import MobileNavbar from "./(user)/components/sidebar/mobile";
+import { homePageItems as items } from "~/lib/data";
 
-import {  Footer, Hero, Navbar } from '~/components';
-import MobileNavbar from './(user)/components/sidebar/mobile';
-
-import { homePageItems as items } from '~/lib/data';
+export const metadata = {
+  title: "NIT Agartala Gymkhana | Official Student Body of NIT Agartala",
+  description:
+    "Official website of NIT Agartala Gymkhana – student council, clubs, cultural and sports activities of National Institute of Technology Agartala.",
+};
 
 const Home = () => {
   return (
     <div>
+      <h1 className="sr-only">NIT Agartala Gymkhana</h1>
+
       <MobileNavbar items={items} />
-      <div className='hidden lg:flex'>
+
+      <div className="hidden lg:flex">
         <Navbar />
       </div>
+
       <Hero />
 
       <Footer />
